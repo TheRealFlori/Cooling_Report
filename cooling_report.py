26a18506bf517608a97531087d380010e5c6a20b
TheRealFlori <flori.herrmann@web.de>
2022-07-26 16:34:14 +0200

    first idk

---
+++ cooling_report.py
@@ -0,0 +1,26 @@
+# streamlit_app.py
+
+import streamlit as st
+import pymongo
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["brk-regenstauf"])
+
+client = init_connection()
+
+# Pull data from the collection.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def get_data():
+    mydb = client["brk-regenstauf"]
+    mycol = mydb["Cooling Reporting"]
+    return items
+
+items = get_data()
+
+# Print results.
+for item in items:
+    st.write(f"{item['name']} has a :{item['pet']}:")
