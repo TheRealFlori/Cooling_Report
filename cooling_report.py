3f678f3f308af48930aa56fc1f62d0f893db3055
TheRealFlori <flori.herrmann@web.de>
2022-07-26 17:04:03 +0200

    Title

--- cooling_report.py
+++ cooling_report.py
@@ -1,13 +1,17 @@
 import streamlit as st # pip install stremlit
 import pymongo # pip install pymongo
 
+import calendar
+import datetime
+
 #--------------------------- SETTINGS ------------------
 page_title = "BRK Regenstauf Temperatur Überwachung"
-page_icon = ""
+page_icon = "Bereitschaftslogo.jpg"
 layout = "centered"
 #-------------------------------------------------------
 
-st.set_page_config(page_title=page_title, layout=layout)
+st.set_page_config(page_title=page_title, page_icon=page_icon layout=layout)
+st.title(page_title + page_icon)
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
