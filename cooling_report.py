74fb1eb92ed602466c17c80e479877fa104d2b2b
TheRealFlori <flori.herrmann@web.de>
2022-08-15 18:47:56 +0200

    Update cooling_report.py

--- cooling_report.py
+++ cooling_report.py
@@ -32,10 +32,10 @@ except Exception as e:
 @st.experimental_memo(ttl=600)
 def get_data():
     db = client["brk-regenstauf"]
-    col = db["Cooling Reporting"].find_one( {}, { "type": fridge})
+    col = db["Cooling Reporting"].find_one( {}, { "type": 'fridge'})
     return col
 
 items = get_data()
 
 # Print results.
-st.write(itemsa)
+st.write(items)
