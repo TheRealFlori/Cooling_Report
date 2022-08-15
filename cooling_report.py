560ee2cabcacbb09a24677fdb9ff1105f9d3da65
TheRealFlori <flori.herrmann@web.de>
2022-08-15 18:45:42 +0200

    Update cooling_report.py

--- cooling_report.py
+++ cooling_report.py
@@ -18,7 +18,7 @@ st.title(page_title)
 # Uses st.experimental_singleton to only run once.
 @st.experimental_singleton
 def init_connection():
-    return pymongo.MongoClient(**st.secrets["mong"])
+    return pymongo.MongoClient(**st.secrets["mongo"])
 
 try:
     client = init_connection()
